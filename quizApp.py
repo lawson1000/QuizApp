c04867f5966b95933b145e155d7ffcd931c42f58
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-01-12 09:35:59 +0100

    first commit

---
+++ quizApp.py
@@ -0,0 +1,10 @@
+categories = ['Entertainment', 'Polities', 'Sport', 'Beauty', 'Lifestyle']
+print("Please Enter Your Name:")
+user_name = input(">>")
+print(f"{user_name} Welcome to the Ultimate Quiz App"
+      f"\nPlease select these three options to play from using the numbers"
+      f"\n[1] Entertainment"
+      f"\n[2] Polities"
+      f"\n[3] Sport"
+      f"\n[4] Beauty"
+      f"\n[5] Lifestyle")
