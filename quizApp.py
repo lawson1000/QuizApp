73b9fffaaf31b1844365c36e2691a14b6612470c
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-01-14 00:33:55 +0100

    some Error handling

--- quizApp.py
+++ quizApp.py
@@ -1,28 +1,49 @@
 from random import randint
 
-categories = ['Entertainment', 'Polities', 'Sport', 'Beauty', 'Lifestyle']
+# categories = ['Entertainment', 'Polities', 'Sport', 'Beauty', 'Lifestyle']
 print("Please Enter Your Name:")
-user_name = input(">>")
-print(f"Hello {user_name.title()}, Welcome to the Ultimate Quiz App"
-      f"\n[1] Entertainment"
-      f"\n[2] Polities"
-      f"\n[3] Sport"
-      f"\n[4] Beauty"
-      f"\n[5] Lifestyle")
+# error handling for username
+while True:
+    user_name = input(">>").title().strip()
+    while user_name.isalpha() is not True or len(user_name) <=2 or len(user_name) >= 16:
+        print("Enter only Alphabets and Name must be more than 2 letters and less that 16 letters")
+        user_name = input(">>").title().strip()
+
+    while user_name.isalpha() is True and len(user_name) >=3 and len(user_name) <= 15:
+        print(f"Hello {user_name}, Welcome to the Ultimate Quiz App"
+              f"\n[1] Entertainment"
+              f"\n[2] Polities"
+              f"\n[3] Sport"
+              f"\n[4] Beauty"
+              f"\n[5] Lifestyle")
+        break
+    break
 
 print(f"Please select three options from above to play from using their numbers")
-user_pick1 = int(input(">>"))
-user_pick2 = int(input(">>"))
-user_pick3 = int(input(">>"))
-Q_numbers = 1
 
+# Error handling for picking questions
+while True:
+    try:
+        user_pick1 = int(input(">>"))
+        if user_pick1 < 1 or user_pick1 > 5:
+            print("Enter any number between 1 - 5")
+            continue
+        else:
+            break
+    except ValueError:
+        print("Enter any number from the option above")
+# user_pick2 = int(input(">>"))
+# user_pick3 = int(input(">>"))
+
+Q_numbers = 1
 score = 0
 
 
 def entertainment():
       print(f"There are 3 Questions in this Entertainment Session")
       def question1():
-          print(f"Question1\n"
+          global Q_numbers
+          print(f"Question {Q_numbers}\n"
                 f"=============================\n"
                 f"Davido is a Musician?\n"
                 f"[A] True \n[B] False\n"
@@ -41,9 +62,10 @@ def entertainment():
                       f"WRONG\n"
                       f"Your Current Score is {score}\n"
                       f"========================\n")
-
+          Q_numbers+=1
       def question2():
-          print(f"Question2\n"
+          global Q_numbers
+          print(f"Question {Q_numbers}\n"
                 f"=============================\n"
                 f"Who made this viral statement 'E Choke!' ?\n"
                 f"[A] Portable \n[B] Davido\n"
@@ -62,9 +84,11 @@ def entertainment():
                       f"WRONG\n"
                       f"Your Current Score is {score}\n"
                       f"========================\n")
+          Q_numbers +=1
 
       def question3():
-            print(f"Question3\n"
+            global Q_numbers
+            print(f"Question {Q_numbers}\n"
                   f"=============================\n"
                   f"Reekado Banks won the next rated Award in what year?\n"
                   f"[A] 2021 \n[B] 2019\n[C] 2013 \n[D] 2015\n"
@@ -82,9 +106,10 @@ def entertainment():
                       f"WRONG\n"
                       f"Your Current Score is {score}\n"
                       f"========================\n")
-
+            Q_numbers +=1
       def question4():
-          print(f"Question4\n"
+          global Q_numbers
+          print(f"Question {Q_numbers}\n"
                 f"=============================\n"
                 f"Who is the CEO/Owner of YBNL record Label? \n"
                 f"[A] Ycee \n[B] Olamide\n[C] Davido \n[D] Dj Xclusive\n"
@@ -103,6 +128,7 @@ def entertainment():
                       f"WRONG\n"
                       f"Your Current Score is {score}\n"
                       f"========================\n")
+          Q_numbers += 1
       rand_list= []
       for i in range(3):
             while True:
